48d4c7ef46b9c9db1d6fe2751681350d3954d9a5
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-09-22 02:25:41 -0700

    Update Input even and odd numbers.py

--- Input even and odd numbers.py
+++ Input even and odd numbers.py
@@ -1,6 +1,12 @@
-num = int(input("Enter number "))
-if num%2 == int(1):
-    print(num,"is odd")
-elif num%2 == int(0):
-    print(num,"is even")
-elif isdigit
+while True:
+    try:
+        num = int(input("Enter number "))
+        if int(num % 2) == int(1):
+            print(num, "is odd")
+        elif int(num % 2) == int(0):
+            print(num, "is even")
+
+        break
+    except ValueError:
+        print("\nur mom")
+        break
