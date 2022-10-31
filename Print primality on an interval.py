1ceae31965c36bacd39108982b2ef7844c2714fa
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 16:05:55 -0700

    Update Print primality on an interval.py

--- Print primality on an interval.py
+++ Print primality on an interval.py
@@ -1,29 +1,28 @@
-# Primality on an interval
-while True:
-    try:
-        LowerBound = int(input("Prime numbers from:"))
-        UpperBound = int(input("to:"))
-        if UpperBound <= LowerBound:
-            print("Upper bound must be strictly greater than lower bound.")
-        elif LowerBound < 1:
-            print("Lower bound must be a positive integer")
-            break
-        else:
-            i = LowerBound
-            while LowerBound <= i <= UpperBound:
-                HasDivisors = False
-                for j in range(2, i):
-                    if i % j == 0:
-                        HasDivisors = True
-                        break
-                if HasDivisors:
-                    print(i, "is not a prime number.")
-                elif i == 1:
-                    print(i, "is not a prime number")
-                else:
-                    print(i, "is a prime number.")
-                i = i+1
-            break
-    except ValueError:
-        print('Your input must be a positive integer.')
-        break
+# Primality on an interval:
+try:
+    lower_bound = int(input("Prime numbers from: "))
+    upper_bound = int(input("to: "))
+    
+    if upper_bound <= lower_bound:
+        print("Upper bound must be greater than lower bound.")
+        
+    elif lower_bound < 1:
+        print("Lower bound must be a positive integer")
+        
+    else:
+        number = lower_bound
+        while lower_bound <= number <= upper_bound:
+            prime = True
+            for divisor in range(2, number):
+                if number % divisor == 0:
+                    prime = False
+                    break
+
+            if prime and number != 1:
+                print(number, "is a prime number.")
+            else:
+                print(number, "is not a prime number.")
+                
+            number = number + 1
+except ValueError:
+    print('Not an integer.')
