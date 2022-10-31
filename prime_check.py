68086e344517a998cace8ce513715685e7dd40e9
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 15:05:05 -0700

    Update and rename Check primality.py to prime_check.py

---
+++ prime_check.py
@@ -0,0 +1,22 @@
+# Check primality:
+try:
+    number = int(input('Even a positive integer: '))
+
+    if number < 1:
+        print('Input must be greater than zero.')
+        pass
+
+    while True:
+        prime = True
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                prime = False
+        break
+
+    if prime and number != 1:
+        print(number, 'is a prime number.')
+    else:
+        print(number, 'is not a prime number.')
+
+except ValueError:
+    print('Your input must be an integer.')
