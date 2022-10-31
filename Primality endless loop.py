54f421dd944ba9d0a745d0900fc96738654fc491
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 14:32:53 -0700

    Update Primality endless loop.py

--- Primality endless loop.py
+++ Primality endless loop.py
@@ -1,26 +1,25 @@
 # Primality endless loop
-while True:
-    try:
-        starting_point = int(input("Prime numbers from:"))
-        if starting_point < 1:
-            print("Lower bound must be a positive integer")
-            break
-        else:
-            i = starting_point
-            while starting_point <= i:
-                HasDivisors = False
-                for j in range(2, i):
-                    if i % j == 0:
-                        HasDivisors = True
-                        break
-                if HasDivisors:
-                    pass
-                elif i == 1:
-                    pass
-                else:
-                    print(i, "is a prime number.")
-                i = i+1
-            break
-    except ValueError:
-        print('Your input must be a positive integer.')
-        break
+
+try:
+    bound = int(input('Prime numbers from: '))
+        
+    if bound < 1:
+        print('Your input must be greater than zero.')
+        pass
+
+    number = bound
+    while True:
+        has_divisors = False
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                has_divisors = True
+                break
+                    
+        if not has_divisors and number != 1:
+            print(number, 'is prime.')
+                
+                
+        number = number + 1
+        
+except ValueError:
+    print('Your input must be a positive integer.')
