121be279b168bd00fce25a83b213633828530935
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 16:10:22 -0700

    Update parity_interval.py

--- parity_interval.py
+++ parity_interval.py
@@ -1,20 +1,20 @@
 # Even and odd numbers loop:
 try:
-    lower_bound = int(input("Even and odd numbers from: "))
-    upper_bound = int(input("to: "))
+    lower_bound = int(input('Even and odd numbers from: '))
+    upper_bound = int(input('to: '))
     
     if upper_bound <= lower_bound:
-        print("Upper bound must be greater than lower bound.")
+        print('Upper bound must be greater than lower bound.')
     
     elif lower_bound < upper_bound:
-        i = lower_bound
-    while lower_bound <= i <= upper_bound:
-        if i % 2 == 0:
-            print(i, "is even")
+        number = lower_bound
+    while lower_bound <= number <= upper_bound:
+        if number % 2 == 0:
+            print(number, 'is even')
         else:
-            print(i, "is odd")
+            print(number, 'is odd')
             
-        i = i+1
+        number = number + 1
         
 except ValueError:
     print("Your input must be an integer")
