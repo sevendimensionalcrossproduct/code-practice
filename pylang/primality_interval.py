d07110aea1b2c7c36841115585ed2e51241b1397
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 18:57:13 -0700

    Create primality_interval.py

---
+++ pylang/primality_interval.py
@@ -0,0 +1,28 @@
+# Primality on an interval:
+try:
+    lower_bound = int(input('Prime numbers from: '))
+    upper_bound = int(input('to: '))
+    
+    if upper_bound <= lower_bound:
+        print('Upper bound must be greater than lower bound.')
+        
+    elif lower_bound < 1:
+        print('Lower bound must be a positive integer')
+        
+    else:
+        number = lower_bound
+        while lower_bound <= number <= upper_bound:
+            prime = True
+            for divisor in range(2, number):
+                if number % divisor == 0:
+                    prime = False
+                    break
+
+            if prime and number != 1:
+                print(number, 'is a prime number.')
+            else:
+                print(number, 'is not a prime number.')
+                
+            number = number + 1
+except ValueError:
+    print('Error: Not an integer.')
