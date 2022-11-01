27ed0e0e34aa9b8c7fa67b8e65f96a217070b714
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-11-01 12:45:00 -0700

    Update triangle.py

--- pylang/triangle/triangle.py
+++ pylang/triangle/triangle.py
@@ -3,52 +3,24 @@ from tkinter import *
 
 penis = Tk()
 
-penis.iconbitmap('18vxahbvpu1cdjpg.ico') #wont work without ico file
 penis.geometry('360x290')
 penis.title('THIS IS A FUCKING RIGHT TRIANGLE')
+penis.iconbitmap('18vxahbvpu1cdjpg.ico')
 penis.resizable(False, False)
 
 cock = LabelFrame(penis, text='RIGHT TRIANGLE')
 cock.pack(fill='both', expand='yes', padx=10, pady=10)
 
-Label(cock, text='THIS', font=('Arial')).grid(row=1, column=50)
-Label(cock, text='IS', font=('Arial')).grid(row=2, column=50)
-Label(cock, text='A', font=('Arial')).grid(row=3, column=50)
-Label(cock, text='FUCKING', font=('Arial')).grid(row=4, column=50)
-Label(cock, text='RIGHT', font=('Arial')).grid(row=5, column=50)
-Label(cock, text='TRIANGEL', font=('Arial')).grid(row=6, column=50)
-Label(cock, text='↲', font=('Arial')).grid(row=7, column=50)
+vagina = {1:'THIS',2:'IS',3:'A',4:'FUCKING',5:'RIGHT',6:'TRIANGL',7:'↲'} 
 
-Label(cock, text='l', font=('Arial')).grid(row=1, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=2, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=3, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=4, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=5, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=6, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=7, column=0)
-Label(cock, text='l', font=('Arial')).grid(row=8, column=0)
+[Label(cock, text=vagina[description], font=('Arial')).grid(row=description, column=50) for description in range(1,8)]
+
+[Label(cock, text='l', font=('Arial')).grid(row=vertical, column=0) for vertical in range(1,9)]
+
+[Label(cock, text='-', font=('Arial')).grid(row=9, column=horizontal) for horizontal in range(1,10)]
+
+[Label(cock, text='╲', font=('Arial')).grid(row=diagonal, column=diagonal) for diagonal in range(1,9)]
 
 Label(cock, text=' ∟', font=('Arial')).grid(row=9, column=0)
-
-Label(cock, text='-', font=('Arial')).grid(row=9, column=1)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=2)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=3)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=4)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=5)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=6)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=7)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=8)
-Label(cock, text='-', font=('Arial')).grid(row=9, column=9)
-
-
-Label(cock, text='╲', font=('Arial')).grid(row=1, column=1)
-Label(cock, text='╲', font=('Arial')).grid(row=2, column=2)
-Label(cock, text='╲', font=('Arial')).grid(row=3, column=3)
-Label(cock, text='╲', font=('Arial')).grid(row=4, column=4)
-Label(cock, text='╲', font=('Arial')).grid(row=5, column=5)
-Label(cock, text='╲', font=('Arial')).grid(row=6, column=6)
-Label(cock, text='╲', font=('Arial')).grid(row=7, column=7)
-Label(cock, text='╲', font=('Arial')).grid(row=8, column=8)
-Label(cock, text='╲', font=('Arial')).grid(row=8, column=8)
-
+    
 penis.mainloop()
