7d538ca3aa1ec963ac2689d7dcb7d81a5a1dc991
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-10-31 18:54:31 -0700

    Update and rename a to check_parity.py

---
+++ pylang/check_parity.py
@@ -0,0 +1,11 @@
+# Check if input is even or odd:
+try:
+    number = int(input('Enter integer: '))
+        
+    if int(number % 2) == 0:
+        print(number, 'is even.')
+    else:
+        print(number, 'is odd.')
+
+except ValueError:
+    print('Error: Not an integer.')
