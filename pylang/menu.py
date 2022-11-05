3c86543c7f08ea68f97fa0d9634e3db6fd08a31d
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2022-11-04 22:44:46 -0600

    Create menu.py

---
+++ pylang/menu.py
@@ -0,0 +1,58 @@
+print('MENU:')
+menu = {'coffee': 15, 'tea':7,'cheese toast':25,'water':5}
+for ass, poop in menu.items():
+    print(ass ,poop)
+
+try:
+    def choice1():
+        item = input("enter name of item: ")
+        
+        if item in menu:
+            print("the price of ", item, "is", menu[item])
+                
+        else:
+            print('the item "' + item + '" is not in the menu')
+    
+    def choice2():
+        try:
+            budget = int(input('Enter budget:'))
+        
+            if budget < min(menu.values()):
+                print("No items within budget found")
+                
+            else:
+                print("Items within budget:")
+                for ass, sex in menu.items():
+                    if sex <= budget:
+                        print(ass, sex)
+
+        except ValueError:
+            print('Not an integer')
+            
+    def choice3():
+        try:
+            name = str(input("Enter new item name:"))
+            price = int(input("Enter price:"))
+            
+        except ValueError:
+            print("bruh")
+
+    option = int(input('\n1. Find item by name\n2. Find item by price\n3. Add new item\n4. Delete item\n'))
+    
+    if not 1 <= option <= 4:
+        print ("poo")
+        
+    elif option == 1:
+        choice1()
+        
+    elif option == 2:
+        choice2()
+        
+    elif option == 3:
+        choice3()
+        
+    elif option == 4:
+        choice4()
+        
+except ValueError:
+    print("wtf")
