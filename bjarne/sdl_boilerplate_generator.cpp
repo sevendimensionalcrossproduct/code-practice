10adb51e9bb2e1d1a29b1e6f9dcb97a156c57df5
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-10-14 04:33:33 -0700

    Update sdl_boilerplate_generator.cpp

--- bjarne/sdl_boilerplate_generator.cpp
+++ bjarne/sdl_boilerplate_generator.cpp
@@ -3,59 +3,58 @@
 #include <fstream>
 
 std::string generateCode(void){
-	std::string boilerplate = R"(
-	#include <iostream>
-	#include <SDL2/SDL.h>
+	std::string boilerplate = 
+R"(#include <iostream>
+#include <SDL2/SDL.h>
 	
-	int windowWidth = 640;
-	int windowHeight = 480;
+int windowWidth = 640;
+int windowHeight = 480;
 
-	int main(){
-		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-			printf("SDL Initialization Failed: %s\n", SDL_GetError());
-			return 1;
-		}
+int main(){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		printf("SDL Initialization Failed: %s\n", SDL_GetError());
+		return 1;
+	}
 		
-		SDL_Window* window = SDL_CreateWindow("New Window!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, 0); //Change window name here
-		if(!window){
-			printf("Failed to create window: %s\n", SDL_GetError());
-			SDL_Quit();
-			return 1;
-		}
+	SDL_Window* window = SDL_CreateWindow("New Window!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, 0); //Change window name here
+	if(!window){
+		printf("Failed to create window: %s\n", SDL_GetError());
+		SDL_Quit();
+		return 1;
+	}
 
-		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if(!renderer){
-			printf("Failed to create render: %s\n", SDL_GetError());
-			SDL_DestroyWindow(window);
-			SDL_Quit();
-			return 1;
-		}
-
-		SDL_SetRenderDrawColor(renderer,0,0,0,255); //Change background color here
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
-
-		SDL_Event event;
-		bool mainLoop = true;
-
-		while(mainLoop){
-			while(SDL_PollEvent(&event)){
-				if(event.type == SDL_QUIT){
-					mainLoop = false;
-					break;
-				}
-			}
-			//Write your SDL application here
-
-		}
-		
-		SDL_DestroyRenderer(renderer);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(!renderer){
+		printf("Failed to create render: %s\n", SDL_GetError());
 		SDL_DestroyWindow(window);
 		SDL_Quit();
-
-		return 0;
+		return 1;
 	}
-	)";
+
+	SDL_SetRenderDrawColor(renderer,0,0,0,255); //Change background color here
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Event event;
+	bool mainLoop = true;
+
+	while(mainLoop){
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_QUIT){
+				mainLoop = false;
+				break;
+			}
+		}
+		//Write your SDL application here
+
+	}
+		
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+})";
 
 	return boilerplate;
 }
