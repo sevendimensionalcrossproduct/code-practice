e9cb89d337208ad63d1984db14abbd0e70ebda7f
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-10-14 02:15:44 -0700

    Create sdl_boilerplate_generator.cpp

---
+++ bjarne/sdl_boilerplate_generator.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string generateCode(void){
+	std::string boilerplate = R"(
+	#include <iostream>
+	#include <SDL2/SDL.h>
+	
+	int windowWidth = 640;
+	int windowHeight = 480;
+
+	int main(){
+		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+			printf("SDL Initialization Failed: %s\n", SDL_GetError());
+			return 1;
+		}
+		
+		SDL_Window* window = SDL_CreateWindow("New Window!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, 0); //Change window name here
+		if(!window){
+			printf("Failed to create window: %s\n", SDL_GetError());
+			SDL_Quit();
+			return 1;
+		}
+
+		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		if(!renderer){
+			printf("Failed to create render: %s\n", SDL_GetError());
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+			return 1;
+		}
+
+		SDL_SetRenderDrawColor(renderer,0,0,0,255); //Change background color here
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+
+		SDL_Event event;
+		bool mainLoop = true;
+
+		while(mainLoop){
+			while(SDL_PollEvent(&event)){
+				if(event.type == SDL_QUIT){
+					mainLoop = false;
+					break;
+				}
+			}
+			//Write your SDL application here
+
+		}
+		
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+		return 0;
+	}
+	)";
+
+	return boilerplate;
+}
+
+void saveFile(const std::string& directory, const std::string& fileName, const std::string& code){
+	std::ofstream fileOutput(directory + '/' + fileName + ".cpp");
+
+	if (fileOutput.is_open()){
+		fileOutput << code;
+		fileOutput.close();
+		std::cout << "File saved as " << fileName << " in directory: " << directory << std::endl;
+	} else {
+		std::cerr << "Error: Unable to create/open file." <<std::endl;
+	}
+}
+
+
+int main(){
+	std::string directory, fileName;
+
+	std::cout << "Enter directory: ";
+	std::cin >> directory;
+
+	std::cout << "Enter filename: ";
+	std::cin >> fileName;
+
+	std::string code = generateCode();
+
+	saveFile(directory, fileName, code);
+
+	return 0;
+}
