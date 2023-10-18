f1ae6c205b3f707523f2ea572be3d23dc3e6c65e
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-10-18 02:23:24 -0700

    Update sevendimensionalcrossproduct.cpp

--- bjarne/sevendimensionalcrossproduct.cpp
+++ bjarne/sevendimensionalcrossproduct.cpp
@@ -1,49 +1,48 @@
+#include <cstdlib>
 #include <iostream>
-#include <ostream>
-#include <stdexcept>
 #include <vector>
-#include <iterator>
 
 using namespace std;
 
 #define START(n) ((i + n) % 7)
 
-std::vector<double> seven_dimensional_cross_product(const std::vector<double>& x, const std::vector<double>& y){
+std::vector<double> seven_dimensional_cross_product(const std::vector<double>& x, const std::vector<double>& y){ //calculates x × y
 	if (x.size() != 7 || y.size() != 7){
-		throw std::invalid_argument("Input vectors must be seven dimensional.");
+		cerr << ("Input vectors must be seven dimensional.") << endl;
+		exit(1);
 	}
 
 	std::vector<double> result(7,0.0);
 	
 	for(int i = -1; i < 6; i++){  
-	result[START(1)] = (x[START(2)]*y[START(4)] - x[START(4)]*y[START(2)] + x[START(3)]*y[START(7)] - x[START(7)]*y[START(3)] + x[START(5)]*y[START(6)] - x[START(6)]*y[START(5)]);
+	result[START(1)] = (x[START(2)]*y[START(4)] - x[START(4)]*y[START(2)] + x[START(3)]*y[START(7)] - x[START(7)]*y[START(3)] + x[START(5)]*y[START(6)] - x[START(6)]*y[START(5)]); //lmao
 	}
 
 	return result;
 }
 
-
-
-void test(int n){
-	for(int i = -1; i < 6; i++){
-		//std::cout << (i + 1) % 7 + 1 <<std::endl;
-		//std::cout <<(i + 3) % 7 + 1 << std::endl;
-		std::cout <<(i + n) % 7 << std::endl;
-		//std::cout <<(i + 6) % 7 + 1 << std::endl;
-		
-		
+double seven_dimensional_dot_product(const vector<double>& x, const vector<double>& y){ //calculates x · y
+	if (x.size() != 7 || y.size() != 7){
+		throw std::invalid_argument("Input vectors must be seven dimensional.");
+		exit(1);
 	}
+
+	double result;
+
+	for(int i = 0; i < 7; i++){
+		result += x[i] * y[i];  
+	}
+
+	return result;
 }
 
 int main(void){
-	std::vector<double> x{0.0,0.0,0.0,0.0,0.0,0.0,1.0};
-	std::vector<double> y{0.0,1.0,0.0,0.0,0.0,0.0,0.0};
+	std::vector<double> x{1.0,1.0,0.0,0.0,0.0,1.0,1.0};
+	std::vector<double> y{0.0,1.0,0.0,0.0,1.0,0.0,0.0};
 	
 	std::vector<double> result = seven_dimensional_cross_product(x,y);
+	double xResult = seven_dimensional_dot_product(result,result);
 
-	int index = 1;
-	for(int element : result){
-		cout << index << ": " << element << endl;
-		index++;
-	}
+
+	cout << xResult << endl;
 }
