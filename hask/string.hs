4f350b4bb46a73b12bd4b85e5b5d134347a362a3
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-11-30 00:50:10 -0800

    rework

--- hask/string.hs
+++
@@ -1,9 +0,0 @@
-import System.IO
-
-main :: IO ()
-main =  putStr "What is your first name? " >>
-        getLine >>= \ first ->
-        putStr "And your last name? " >>
-        getLine >>= \ last ->
-        let full = first ++ " " ++ last
-        in putStrLn ("Pleased to meet you, " ++ full ++ "!")
