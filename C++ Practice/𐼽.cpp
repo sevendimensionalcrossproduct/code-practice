18a941dad9472172b7db6dfb61b8346bca33ec84
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-11-30 00:45:36 -0800

    rework

---
+++ C++ Practice/𐼽.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	while(true){
+		cout << " 𐼽" << endl;
+	}
+}
--- lisp (structure and interpretation of ur mom)/sum_of_squares.scm
+++
@@ -1,7 +0,0 @@
-(define (square x) 
-  (* x x))
-
-(define (sum-of-squares x y)
-  (+ (square x) (square y)))
-
-(sum-of-squares x y)
