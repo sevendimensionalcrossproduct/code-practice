918605313ba150dd2f0e399b584d3a2f0ab1cb14
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-01 04:13:06 -0800

    something

--- Functional Programming/Haskell/src/MyLib.hs
+++ Functional Programming/Haskell/src/MyLib.hs
@@ -1,5 +1,22 @@
 module MyLib (someFunc) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Index (stringToIndices)
 
+someFunc :: IO ()
+someFunc = 
+  putStrLn "someFunc" >>
+  someFunc
+
+
+square :: Int -> Int
+square = \ x -> x * x 
+
+calculate :: IO ()
+calculate =
+  putStr "Number: " >>
+  getLine >>= \ input ->
+
+  let number = read input :: Int in
+  
+  putStr (input <> stringToIndices "2" <> " = ") >>
+  print (square number)
