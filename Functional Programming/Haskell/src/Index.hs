1255a4c436d82cba9a0e7256bbe56a515e0b6789
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-01 00:35:17 -0800

    Create Index.hs

---
+++ Functional Programming/Haskell/src/Index.hs
@@ -0,0 +1,24 @@
+module Index where
+
+superindex :: Int -> Char 
+superindex input =
+   case input of
+    0 -> '⁰'
+    1 -> '¹'
+    2 -> '²'
+    3 -> '³'
+    4 -> '⁴'
+    5 -> '⁵'
+    6 -> '⁶'
+    7 -> '⁷'
+    8 -> '⁸'
+    9 -> '⁹'
+    _ -> ' '
+
+stringToIndices :: String -> String
+stringToIndices = map (\c ->superindex (read [c] :: Int))
+
+getIndex :: IO ()
+getIndex =
+  putStr "Enter integer: " >>
+  getLine >>= \ input ->putStrLn (stringToIndices input)
