c6a6d246a55d62fc5559b5bd7e6dc4951822dc23
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-02 23:45:34 -0800

    Update Dual.hs

--- Functional Programming/Haskell/src/Dual.hs
+++ Functional Programming/Haskell/src/Dual.hs
@@ -10,3 +10,12 @@ getEpsilon (DualNumber _ y) = y
 
 add :: DualNumber ->DualNumber -> DualNumber
 add (DualNumber x1 y1) (DualNumber x2 y2) =  DualNumber (x1 + x2) (y1 + y2)
+
+substr :: DualNumber -> DualNumber -> DualNumber
+substr (DualNumber x1 y1) (DualNumber x2 y2) = DualNumber (x1 - x2) (y1 -y2)
+
+mult :: DualNumber -> DualNumber -> DualNumber
+mult (DualNumber x1 y1) (DualNumber x2 y2) = DualNumber (x1 * x2) ((x1 * y2) + (y1 * x2))
+
+div :: DualNumber -> DualNumber -> DualNumber
+div (DualNumber x1 y1) (DualNumber x2 y2) = DualNumber (x1 / x2) (((y1 * x2) - (x1 * y2)) / (x2 * x2))
