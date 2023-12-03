0795fd9d350205920a4111d7d86e4ed35783424d
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-03 04:30:25 -0800

    Update Dual.hs

--- Functional Programming/Haskell/src/Dual.hs
+++ Functional Programming/Haskell/src/Dual.hs
@@ -1,7 +1,9 @@
 module Dual where
---ε
+
 data DualNumber = DualNumber Double Double
 
+
+{--Instantiation of Dual Numbers--}
 instance Show DualNumber where
     show (DualNumber x y)
         | x == 0 && y == 0.0 = "0" 
@@ -21,12 +23,15 @@ instance Show DualNumber where
         | otherwise = "0"
         
 
+{--Getter Functions--}
 getReal :: DualNumber -> Double
 getReal (DualNumber x _) = x 
 
 getEpsilon :: DualNumber -> Double
 getEpsilon (DualNumber _ y) = y
 
+
+{--Basic Operations--}
 add :: DualNumber ->DualNumber -> DualNumber
 add (DualNumber x1 y1) (DualNumber x2 y2) =  DualNumber (x1 + x2) (y1 + y2)
 
@@ -38,3 +43,35 @@ mult (DualNumber x1 y1) (DualNumber x2 y2) = DualNumber (x1 * x2) ((x1 * y2) + (
 
 div :: DualNumber -> DualNumber -> DualNumber
 div (DualNumber x1 y1) (DualNumber x2 y2) = DualNumber (x1 / x2) (((y1 * x2) - (x1 * y2)) / (x2 * x2))
+
+
+{--Elementary Operations--}
+cot :: Double ->Double
+cot x = 1 / tan x
+
+sec :: Double ->Double 
+sec x = 1 / cos x
+
+csc :: Double ->Double
+csc x = 1 / sin x
+
+acot :: Double ->Double
+acot x
+  | isInfinite x = pi / 2 * signum x
+  | x == 0 = pi / 2
+  | x < 0 = atan(1/x) + pi
+  | x > 0 = atan(1/x)
+  | otherwise = 0/0
+
+dnSin :: DualNumber -> DualNumber 
+dnSin z = DualNumber (sin(getReal z)) (cos (getReal z) * getEpsilon z)
+
+dnCos :: DualNumber -> DualNumber
+dnCos z = DualNumber (cos(getReal z)) (-sin(getReal z) * getEpsilon z)
+
+dnTan :: DualNumber -> DualNumber
+dnTan z = DualNumber (tan(getReal z)) (getEpsilon z * (sec(getReal z) ** 2))
+
+dnCot :: DualNumber -> DualNumber
+dnCot z = DualNumber (cot (getReal z)) (-getEpsilon z)
+
