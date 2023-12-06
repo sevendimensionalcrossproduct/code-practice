0b113e202e594c0f584da5c308dffa5850b445b0
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-05 22:26:24 -0800

    Update Fourier.hs

--- Functional Programming/Haskell/src/Fourier.hs
+++ Functional Programming/Haskell/src/Fourier.hs
@@ -5,35 +5,39 @@ import Index (stringToIndices)
 newtype Polynomial = Polynomial [Double]
 
 instance Show Polynomial where
-    show (Polynomial xs) = showPolynomial (zip [1..] xs) where
+    show (Polynomial xs) = showPolynomial (zip [0..] xs) where
 
         showPolynomial :: [(Integer, Double)] -> String
         showPolynomial [] = ""
         showPolynomial ((i, a):rest) = showStream i a ++ showPolynomial rest
 
-        formatX :: Double -> String
-        formatX a
+        showStream :: Integer -> Double -> String
+        showStream i a
             | a == 0 = ""
-            | otherwise = "x"
+            | otherwise = getSign a ++ formatCoefficient a ++ formatX i a ++ formatIndex i
+
+        getSign :: Double -> String
+        getSign x
+            | signum x == -1 = " - "
+            | signum x == 1 = " + "
+            | otherwise = ""
 
         formatCoefficient :: Double -> String
         formatCoefficient a
             | isIntegral a = show (abs (round a) :: Int)
             | otherwise = show (abs a)
 
+        formatX :: Integer ->Double -> String
+        formatX i a
+            | a == 0 || i == 0 = ""
+            | otherwise = "x"
+
         isIntegral :: Double -> Bool
         isIntegral x = snd (properFraction x :: (Integer, Double)) == 0
 
-        getSign :: Double -> String
-        getSign x
-            | signum x == -1 = "-"
-            | signum x == 1 = "+"
-            | otherwise = ""
-
         formatIndex :: Integer -> String
-        formatIndex i = stringToIndices (show (i - 1))
+        formatIndex i
+            | i == 0 || i == 1 = ""
+            | otherwise = stringToIndices (show i)
 
-        showStream :: Integer -> Double -> String
-        showStream i a
-            | a == 0 = ""
-            | otherwise = getSign a ++ formatCoefficient a ++ formatX a ++ formatIndex i
+
