24210a0e5246380fcd3ca93529b399b0e8dba09e
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-05 21:25:21 -0800

    Update Fourier.hs

--- Functional Programming/Haskell/src/Fourier.hs
+++ Functional Programming/Haskell/src/Fourier.hs
@@ -5,23 +5,35 @@ import Index (stringToIndices)
 newtype Polynomial = Polynomial [Double]
 
 instance Show Polynomial where
-    show (Polynomial xs) = showPolynomial (zip [1..] xs)
-        where
-            showPolynomial :: [(Int, Double)] -> String
-            showPolynomial [] = ""
-            showPolynomial [(i, a)] = getSign a ++ formatTerm a i
-            showPolynomial ((i, a):rest) = getSign a ++ formatTerm a i ++ showPolynomial rest
+    show (Polynomial xs) = showPolynomial (zip [1..] xs) where
 
-            formatTerm :: Double -> Int -> String
-            formatTerm a i
-                | isIntegral a = show (abs (round a) :: Int) ++ "x" ++ stringToIndices (show (i-1))
-                | otherwise = show (abs a) ++ "x" ++ stringToIndices (show (i-1))
+        showPolynomial :: [(Integer, Double)] -> String
+        showPolynomial [] = ""
+        showPolynomial ((i, a):rest) = showStream i a ++ showPolynomial rest
 
-            isIntegral :: Double -> Bool
-            isIntegral x = snd (properFraction x :: (Integer, Double)) == 0
+        formatX :: Double -> String
+        formatX a
+            | a == 0 = ""
+            | otherwise = "x"
 
-            getSign :: Double ->String
-            getSign x 
-                | signum x == -1 = "-"
-                | signum x == 1 = "+"
-                | otherwise = ""
+        formatCoefficient :: Double -> String
+        formatCoefficient a
+            | isIntegral a = show (abs (round a) :: Int)
+            | otherwise = show (abs a)
+
+        isIntegral :: Double -> Bool
+        isIntegral x = snd (properFraction x :: (Integer, Double)) == 0
+
+        getSign :: Double -> String
+        getSign x
+            | signum x == -1 = "-"
+            | signum x == 1 = "+"
+            | otherwise = ""
+
+        formatIndex :: Integer -> String
+        formatIndex i = stringToIndices (show (i - 1))
+
+        showStream :: Integer -> Double -> String
+        showStream i a
+            | a == 0 = ""
+            | otherwise = getSign a ++ formatCoefficient a ++ formatX a ++ formatIndex i
