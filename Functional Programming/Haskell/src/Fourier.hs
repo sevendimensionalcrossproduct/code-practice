cabd856bd0c140c5c50152cff2ee214380a48c64
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-05 19:14:06 -0800

    Update Fourier.hs

--- Functional Programming/Haskell/src/Fourier.hs
+++ Functional Programming/Haskell/src/Fourier.hs
@@ -9,13 +9,19 @@ instance Show Polynomial where
         where
             showPolynomial :: [(Int, Double)] -> String
             showPolynomial [] = ""
-            showPolynomial [(i, a)] = formatTerm a i
-            showPolynomial ((i, a):rest) = formatTerm a i ++ " + " ++ showPolynomial rest
+            showPolynomial [(i, a)] = getSign a ++ formatTerm a i
+            showPolynomial ((i, a):rest) = getSign a ++ formatTerm a i ++ showPolynomial rest
 
             formatTerm :: Double -> Int -> String
             formatTerm a i
-                | isIntegral a = show (round a :: Int) ++ "x" ++ stringToIndices (show i)
-                | otherwise = show a ++ "x" ++ stringToIndices (show i)
+                | isIntegral a = show (abs (round a) :: Int) ++ "x" ++ stringToIndices (show (i-1))
+                | otherwise = show (abs a) ++ "x" ++ stringToIndices (show (i-1))
 
             isIntegral :: Double -> Bool
             isIntegral x = snd (properFraction x :: (Integer, Double)) == 0
+
+            getSign :: Double ->String
+            getSign x 
+                | signum x == -1 = "-"
+                | signum x == 1 = "+"
+                | otherwise = ""
