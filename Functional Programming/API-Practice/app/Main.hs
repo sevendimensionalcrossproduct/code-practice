d78faffc44eb267035f07cdd7b2ad41405db00c2
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-12-10 23:45:27 -0800

    sdfsgsdgfdsgf

--- Functional Programming/API-Practice/app/Main.hs
+++ Functional Programming/API-Practice/app/Main.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = scotty 3001 (
+  get "/greet/:name" (captureParam "name" >>= \name -> text ("Hello, " <> name <> "!")) >>
+  get "/hello" (text "shalom")
+  )
